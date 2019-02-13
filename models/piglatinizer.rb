6d94779f51bd18726e661591d5b9224ea915d2d3
Tyler Sell <tylerselldesign@gmail.com>
2019-02-13 04:12:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,12 @@
 class PigLatinizer
   
   def self.piglatinize(word)
+    # if starts with vowel add w to word
+    if vowel?(word[0])
+      word = word + "w"
+    elsif !vowel?(word[0]) && !vowel?(word[1]) && !vowel?(word[2])
+      word = 
+    
     
   end
   
