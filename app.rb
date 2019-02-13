4c8d06259868aff47ab70eb5c7934a578f53c4fc
Tyler Sell <tylerselldesign@gmail.com>
2019-02-13 05:21:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do 
-    erb :user
+    erb :user_input
+  end
+  
+  post '/piglatinize' do 
+    word = PigLatinizer.new
+    @piglatin = word.
 end
