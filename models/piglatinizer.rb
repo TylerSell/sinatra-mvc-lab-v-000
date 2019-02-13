a3c9b5375fb3dc8fcc862bef5714830dfbe5adcd
Tyler Sell <tylerselldesign@gmail.com>
2019-02-13 04:34:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,25 @@
 class PigLatinizer
   
-  def self.piglatinize(word)
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def piglatinize(word)
     # if starts with vowel add w to word
     if vowel?(word[0])
       word = word + "w"
-    else !vowel?(word[0]) && !vowel?(word[1]) && !vowel?(word[2])
+    elsif !vowel?(word[0]) && !vowel?(word[1]) && !vowel?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
+    elsif 
     end
     
+  end
+
+  def vowel?(word)
+    word.downcase 
+    word.match(/aeiou/)
   end
   
 end
